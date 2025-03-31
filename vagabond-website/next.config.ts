1bbf22a779af4243784f0ec4cdec22c000d51b2b
reneedaos <43126993+reneedaos@users.noreply.github.com>
2025-03-30 22:18:55 -0500

    updated next config

--- vagabond-website/next.config.ts
+++ vagabond-website/next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
-};
-
-module.exports = {
+  output: 'export',
   distDir: 'dist',
 };
+
 export default nextConfig;
